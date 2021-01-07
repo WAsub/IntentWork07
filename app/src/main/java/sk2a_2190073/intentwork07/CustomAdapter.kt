85c292d1934f7c00c99e6db57841c9390d8632ff
WakanaArimoto <2190073@ecccomp.com>
2021-01-07 15:58:32 +0900

    no message

---
+++ app/src/main/java/sk2a_2190073/intentwork07/CustomAdapter.kt
@@ -0,0 +1,113 @@
+package sk2a_2190073.intentwork07
+
+import android.app.Activity
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+
+class CustomAdapter(private val activity: Activity,private val items: List<MainActivity.HMdata>) : BaseAdapter() {
+
+    private class ViewHolder(row: View) {
+        val numberText = row.findViewById(R.id.lv_memo) as TextView
+        val nameText  = row.findViewById(R.id.btn_del) as Button
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view: View
+        val viewHolder: ViewHolder
+        if (convertView == null) {
+            val inflater = activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            view = inflater.inflate(R.layout.list_layout, convertView)
+            viewHolder = ViewHolder(view)
+            view.tag = viewHolder
+        } else {
+            view = convertView
+            viewHolder = view.tag as ViewHolder
+        }
+
+        val memberHM = items[position]
+        viewHolder.numberText.text = memberHM.number
+        viewHolder.numberText.setOnClickListener(){
+            (parent as ListView).performItemClick(view , position , getItemId(R.id.lv_memo))
+        }
+        viewHolder.nameText.text = memberHM.name
+        viewHolder.nameText.setOnClickListener(){
+            (parent as ListView).performItemClick(view , position , getItemId(R.id.btn_del))
+        }
+
+        return view
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getItem(position: Int): Any {
+        return items[position]
+    }
+
+    override fun getCount(): Int {
+        return items.size
+    }
+}
+//import android.content.Context
+//import android.media.SoundPool
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import android.view.animation.AlphaAnimation
+//import android.widget.*
+//import android.widget.BaseAdapter
+//
+//
+//class CustomAdapter(
+//    private val context: Context,
+//    private var inflater: LayoutInflater,
+//    private var layoutId: Int,
+//    private var yz: List<MainActivity.Buttons>
+//) : BaseAdapter()  {
+//    internal data class ViewHolder(val _memo:TextView,val _del:Button)
+//
+//    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+//        var view = convertView
+//        val viewHolder : ViewHolder
+//        if (convertView == null) {
+//            view = inflater.inflate(layoutId,parent,false)
+//            viewHolder = ViewHolder(view.findViewById(R.id.lv_memo),view.findViewById(R.id.btn_del))
+//            view.tag = viewHolder
+//        } else {
+//            viewHolder = view!!.tag as ViewHolder
+//        }
+//        viewHolder._memo.setText(yz[position].memo)
+////        val soundId = soundPool.load(context, yz[position].sound, 1)
+//        viewHolder._del.setText(yz[position].del)
+////        fadeout(viewHolder.image,0)
+//        viewHolder._del.setOnClickListener() {
+////            soundPool.play(soundId, 1.0f, 1.0f, 0, 0, 1.0f)
+////
+////            fadeout(viewHolder.image,3000)
+//        }
+//        return view!!
+//    }
+//
+//    override fun getCount(): Int {
+//        return yz.size
+//    }
+//
+//    override fun getItem(position: Int): Any {
+//        return position
+//    }
+//
+//    override fun getItemId(position: Int):Long {
+//        return position.toLong()
+//    }
+//
+//}
+//private fun fadeout(imageView:ImageView,Duration:Long){
+//    val alphaFadeOut = AlphaAnimation(1.0f,0.0f)
+//    alphaFadeOut.setDuration(Duration)
+//    alphaFadeOut.setFillAfter(true)
+//    imageView.startAnimation(alphaFadeOut)
+//}
--- app/src/main/java/sk2a_2190073/intentwork07/MainActivity.kt
+++ app/src/main/java/sk2a_2190073/intentwork07/MainActivity.kt
@@ -1,12 +1,60 @@
 package sk2a_2190073.intentwork07
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val TAG: String = "toridge.main"
+
+
+    data class HMdata(val number: String, val name: String)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listView = findViewById<ListView>(R.id.lvMain)
+        val listHM: List<HMdata> = getHMList()
+
+        val adapter = CustomAdapter(this,listHM)
+        listView.adapter = adapter
+        listView.onItemClickListener = LIClick()
+        val toast = Toast.makeText(applicationContext, "が押されました。", Toast.LENGTH_LONG)
+              toast.show()
+    }
+
+    private inner class LIClick: AdapterView.OnItemClickListener{
+        override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+            when (id.toInt()) {
+                R.id.btn_del -> {
+                    //確認ダイアログ表示
+                    val toast = Toast.makeText(applicationContext, "ボタンが押されました。", Toast.LENGTH_LONG)
+                    toast.show()
+                }
+                R.id.lv_memo -> {
+                    //確認ダイアログ表示
+                    val toast = Toast.makeText(applicationContext, "リストが押されました。", Toast.LENGTH_LONG)
+                    toast.show()
+                }
+            }
+        }
+    }
+
+    private fun getHMList(): MutableList<HMdata> {
+        val list: MutableList<HMdata> = mutableListOf()
+
+
+        list.add(HMdata("AAaaaaa", "++"))
+        list.add(HMdata("AAaaaaa", "++"))
+
+        return list
     }
 }
